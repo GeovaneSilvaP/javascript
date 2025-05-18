85adfe1ca7edba9ec89fcf5ca958be0fbe5c7eae
GeovaneSilvaP <geovanesil45sil@gmail.com>
2025-05-17 21:40:41 -0300

    aula

---
+++ aulas-de-js/codigoJS/aula02.js
@@ -0,0 +1,22 @@
+//CRIAR VARIAVESIS e atribuir valores
+let mensagem = "Minha mensagem";
+let mensagem2 = 'Outra mensagem';
+let meuPeso = 65.5;
+let minhaAutura = 1.75;
+let minhaIdade = 23;
+let ehDoador = false;
+let teste = null;
+
+//CRIAR CONSTANTES e atribuir valor
+const PA = 3.6873;
+const TAXA = 0.5;
+
+teste = 'algum conteudo';
+
+//ESCREVER O CONTEUDO DE VALORES E CONSTANTES
+document.write(mensagem);
+document.write(mensagem2);
+document.write(meuPeso);
+document.write(minhaIdade);
+document.write(PA);
+document.write(TAXA);
