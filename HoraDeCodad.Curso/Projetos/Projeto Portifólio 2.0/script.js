a11b4a4d229c6878f884b29dd811e3ef5b0b17d7
GeovaneSilvaP <geovanesil45sil@gmail.com>
2025-06-21 11:46:03 -0300

    projeto

--- HoraDeCodad.Curso/Projetos/Projeto Portifólio 2.0/script.js
+++ HoraDeCodad.Curso/Projetos/Projeto Portifólio 2.0/script.js
@@ -1,28 +1,30 @@
-const btnSobre = document.querySelector("#btn-sobre");
-const sobre = document.querySelector("#sobre");
+//click menu
+
 const especialidade = document.querySelector("#btn-especialidade");
 const btnEspecialidade = document.querySelector("#btn-btn-hability")
-const container = document.querySelector("#container-imgs");
-const sessao = document.querySelector("#gatilho");
+const sobreMin = document.querySelector("#text-sobre");
+const btnsobre = document.querySelector("#btn-sobre");
 
-//click menu
-btnSobre.addEventListener("click", () => {
-  sobre.scrollIntoView({ behavior: "smooth" });
-});
 
 btnEspecialidade.addEventListener("click", () => {
   especialidade.scrollIntoView({ behavior: "smooth" });
 });
 
+btnsobre.addEventListener("click", () =>{
+  sobreMin.scrollIntoView({behavior: "smooth"});
+})
 
 
 // Seleciona o container e a sessão
+const container = document.querySelector("#container-imgs");
+const sessao = document.querySelector("#gatilho");
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       container.classList.add('visivel');
     } else {
-      container.classList.remove('visivel')
+      container.classList.remove('visivel');
     }
   });
 }, {
@@ -111,4 +113,39 @@ document.addEventListener('DOMContentLoaded', () => {
   addMouseInteraction();
 });
 
+//LOGO
 
+
+const logo = document.querySelector('.logo');
+    const button = document.getElementById('rotateBtn');
+    let rotations = 0;
+
+    // Bouncing animation (loop)
+    anime({
+      targets: '.logo',
+      scale: [
+        { value: 1.25, duration: 1000, easing: 'easeInOutQuad' },
+        { value: 1, duration: 1000, easing: 'easeOutElastic(1, .5)' }
+      ],
+      loop: true,
+      direction: 'alternate',
+      delay: 250
+    });
+
+//img gato
+const gato = document.querySelector(".img-sobre");
+const gatoPonto = document.querySelector("#gatoPonto");
+
+const observeGato = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      gato.classList.add('visivel');
+    } else {
+      gato.classList.remove('visivel');
+    }
+  });
+}, {
+  threshold: 0.5
+});
+
+observeGato.observe(gatoPonto);
